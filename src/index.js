18b6819e51ced4d6f75f95e6b66bf49e9a7668b1
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-09-03 18:50:45 +0300

    one more try

--- src/index.js
+++ src/index.js
@@ -2,83 +2,110 @@ import { fetchImages } from './js/pixabayAPI';
 import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import axios from "axios";
+
+// const elements = {
+//     body : document.querySelector('body'),
+//     searchForm : document.querySelector('.search-form'),
+//     input : document.querySelector('.form-input'),
+//     searchBtn : document.querySelector('.search-btn'),
+//     gallery : document.querySelector('.gallery'),
+//     loadMoreBtn : document.querySelector('.load-more'),
+// }  -- гублю слово «elements», тому вирішила так
 
 
-const elements = {
-    body : document.querySelector('body'),
-    searchForm : document.querySelector('.search-form'),
-    input : document.querySelector('.form-input'),
-    searchBtn : document.querySelector('.search-btn'),
-    gallery : document.querySelector('.gallery'),
-    loadMoreBtn : document.querySelector('.load-more'),
-}
+const searchForm = document.getElementById("search-form");
+const gallery = document.querySelector(".gallery");
+const loadMoreBtn = document.querySelector(".load-more");
 
-// export default { 
-//     beforeCreate() { 
-//       this.showHideSpinner = true; 
-//     }, 
-//     mounted() { 
-//       this.showHideSpinner = false; 
-//     }, 
-//     data() { 
-//       return { 
-//         showHideSpinner: true 
-//       }; 
-//     } 
-//   }; 
+const lightbox = new SimpleLightbox(".photo-card a"); 
+
+const API_KEY = "39213974-396d72fb1f832236dc1554890"; 
+const BASE_URL = "https://pixabay.com/api/";
+const PER_PAGE = 40;
+// const page = 1;
+// const query = '';
 
 let page = 1;
 
-elements.searchForm.addEventListener('submit', handlerSubmit);
-function handlerSubmit(evt) {
-evt.preverentDefault();
-const searchQuery = evt.target.searchQuery.value;
-if (searchQuery.trim() === "") {
+searchForm.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const searchQuery = event.target.searchQuery.value;
+  if (searchQuery.trim() === "") {
     Notiflix.Notify.failure("Please enter a search query.");
-    return
-}
-page = 1;
-elements.gallery.innerHTML = "";
-fetchImages(searchQuery);
+    return;
+  }
+  page = 1;
+  gallery.innerHTML = ""; 
+  fetchImages(searchQuery);
 }
 
-elements.loadMoreBtn.addEventListener('click', loadMoreImages);
+loadMoreBtn.addEventListener("click", loadMoreImages);
 function loadMoreImages() {
-    page +=1;
-    const searchQuery = searchForm.searchQuery.value;
-    fetchImages(searchQuery);
+  page += 1;
+  const searchQuery = searchForm.searchQuery.value;
+  fetchImages(searchQuery);
 }
 
 
+async function fetchImages(searchQuery) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: searchQuery,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: "true",
+        per_page: PER_PAGE,
+        page: page,
+      },
+    })
+    console.log(response);
+    const images = response.data.hits;
+    if (images.length === 0) {
+      Notiflix.Notify.failure(
+        "Sorry, there are no images matching your search query. Please try again."
+      );
+      return;
+    }
 
-function createMarkup(arr) {
-  return arr.map(({ webformatURL, largeImageURL, tags, links, views, comments, downloads }) => `
-  <div class="photo-card">
-  <a href="${largeImageURL} class="photo-card-link"
-      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-    ${likes}</p>
-    <p class="info-item">
-      <b>Views</b>
-    ${views}</p>
-    <p class="info-item">
-      <b>Comments</b>
-   ${comments} </p>
-    <p class="info-item">
-      <b>Downloads</b>
-   ${downloads} </p>
-  </div>
-  </div>`).join('')
+    Notiflix.Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
+    
+    const imageCards = images.map((image) => createImageCard(image));
+    gallery.insertAdjacentHTML("beforeend", imageCards.join(""));
+    
+    lightbox.refresh(); 
   }
   
-  container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+  } catch (error) {
+    console.error(error);
+  }
+
+
+
+function createImageCard(image) {
+  return `
+    <div class="photo-card">
+      <a href="${image.largeImageURL}" class="photo-card-link">
+        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+      </a>
+      <div class="info">
+        <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+        <p class="info-item"><b>Views:</b> ${image.views}</p>
+        <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+        <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
+      </div>
+    </div>
+  `;
+}
+
+
+
+
+
   
-  // console.log(SimpleLightbox);
-  // new SimpleLightbox('.gallery a', {
-  //     captionsData: "alt",
-  //     captionsDelay: 250, });
   
   
 
--- src/js/pixabayAPI.js
+++ src/js/pixabayAPI.js
@@ -1,32 +1,46 @@
-import axios from "axios";
+// import axios from "axios";
 // const axios =  require ("axios").default;
-import Notiflix from "notiflix";
+// import Notiflix from "notiflix";
 
-axios.defaults.baseURL = 'https://pixabay.com/api/';
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-const API_KEY = '39213974-396d72fb1f832236dc1554890';
+// const API_KEY = '39213974-396d72fb1f832236dc1554890';
 // const BASE_URL = "https://pixabay.com/api/";
-const PER_PAGE = 40;
-const page = 1;
-const query = '';
+// const PER_PAGE = 40;
+// const page = 1;
+// const query = '';
 
-axios
-.get('https://pixabay.com/api/?key=39213974-396d72fb1f832236dc1554890&q=text&image_type=photo&orientation=horizontal&safesearch=true',
-{ 
-    //  params = {
-            page: "page",
-            q: "query",
-            per_page: "PER_PAGE",
-            image_type: 'photo',
-            orientation: 'horizontal',
-            safesearch: true,}
-        // }
-        )
-  .then(res => {
-    console.log(res.data);
-  })
-.catch((error) => {
-    console.error(error);
-})
-// fetch('https://pixabay.com/api/?key=39213974-396d72fb1f832236dc1554890&q=text&image_type=photo&orientation=horizontal&safesearch=true')
+// function fetchImages(searchQuery) {
+//   axios
+//     .get(BASE_URL, {
+//       params: {
+//         key: API_KEY,
+//         q: searchQuery,
+//         image_type: "photo",
+//         orientation: "horizontal",
+//         safesearch: "true",
+//         per_page: PER_PAGE,
+//         page: page,
+//       },
+//     })
+//     .then((response) => {
+//       const images = response.data.hits;
+//       if (images.length === 0) {
+//         Notiflix.Notify.failure(
+//           "Sorry, there are no images matching your search query. Please try again."
+//         );
+//         return;
+//       }
 
+//       Notiflix.Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
+      
+//       const imageCards = images.map((image) => createImageCard(image));
+//       gallery.insertAdjacentHTML("beforeend", imageCards.join(""));
+      
+//       lightbox.refresh(); // Оновлення галереї SimpleLightbox
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// }
+//   export { fetchImages };
